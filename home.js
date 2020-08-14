3d9f693952e7fdaf1cef45bd09d42faff6de525c
Abhishek Choudhury <choudhuryabhishek76@gmail.com>
2020-08-14 16:19:16 +0530

    14th Aug 2020

--- home.js
+++ home.js
@@ -1,7 +1,24 @@
 
-console.log('hello')
+/* console.log('hello') */
 // alert("Dont fuck it")
 
-var age = prompt("What is your age ? ")
+/* var age = prompt("What is your age ? ")
 
-document.getElementById('myHeading').innerHTML = age
+document.getElementById('myHeading').innerHTML = age */
+
+
+function username(name) {
+
+    var name = prompt("Enter your name ")
+
+    document.getElementById('myHeading').innerHTML = "Hello Mr."+name
+
+}
+
+username(name)
+
+
+//Array
+let fruits = ['apple' , 'banana', 'mango' ]
+
+console.log(fruits)
